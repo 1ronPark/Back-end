e729d77f5fc1168f1268cbef193f2f20f59a451e
HarryBae1011 <06harry@naver.com>
2025-07-11 20:44:20 +0900

    feat: tools

--- src/main/java/umc/lightup/member/enums/Role.java
+++ src/main/java/umc/lightup/member/enums/Role.java
@@ -1,6 +1,7 @@
 package umc.lightup.member.enums;
 
 public enum Role {
+    ADMIN, //관리자
     LEADER, // 팀원을 구하는 유저
     TEAMMATE // 팀을 구하는 유저
 }
--- src/main/java/umc/lightup/tool/enums/ToolType.java
+++ src/main/java/umc/lightup/tool/enums/ToolType.java
@@ -1,5 +1,5 @@
 package umc.lightup.tool.enums;
 
 public enum ToolType {
-    SOFTWARE, HARDWARE // 실제 타입에 맞게 추가
+    FRONTEND, BACKEND, DESIGN, COLLABORATION, LANGUAGE, DATABASE, DEVOPS
 }
