47fa62a638a1c830c0282abd82ce4a4468994101
choikyungsoo <ryan1954@naver.com>
2025-07-18 23:24:41 +0900

    feat: api에 authentication 추가

--- src/main/java/umc/lightup/notification/controller/NotificationRestController.java
+++ src/main/java/umc/lightup/notification/controller/NotificationRestController.java
@@ -2,11 +2,15 @@ package umc.lightup.notification.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import umc.lightup.api.ApiResponse;
+import umc.lightup.member.domain.Member;
+import umc.lightup.member.service.MemberCommandService;
 import umc.lightup.notification.converter.NotificationConverter;
 import umc.lightup.notification.domain.Notification;
 import umc.lightup.notification.dto.NotificationResponseDTO;
@@ -18,14 +22,20 @@ import umc.lightup.notification.service.NotificationQueryService;
 public class NotificationRestController {
 
   private final NotificationQueryService notificationQueryService;
+  private final MemberCommandService memberCommandService;
 
   @GetMapping("")
-  @Operation(summary = "알림 목록 불러오기 API", description = "수신 받은 알림 조회를 위한 API 이며, 페이징을 포함합니다. query String으로 page 번호와 size 크기 값을 주세요")
+  @Operation(
+          summary = "알림 목록 불러오기 API",
+          description = "수신 받은 알림 조회를 위한 API 이며, 페이징을 포함합니다. query String으로 page 번호와 size 크기 값을 주세요",
+          security = { @SecurityRequirement(name = "JWT TOKEN")}
+  )
   @ApiResponses({
           @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
   })
-  public ApiResponse<NotificationResponseDTO.NotificationListDTO> getNotificationList(@Valid @RequestParam(name= "userId") Long userId, @RequestParam(name = "page") Integer page, @RequestParam(name = "size") Integer size){
-    Page<Notification> notificationList = notificationQueryService.getNotificationList(userId, page, size);
+  public ApiResponse<NotificationResponseDTO.NotificationListDTO> getNotificationList(Authentication authentication, @Valid @RequestParam(name = "page") Integer page, @RequestParam(name = "size") Integer size){
+    Member member = memberCommandService.getMember(authentication.getName());
+    Page<Notification> notificationList = notificationQueryService.getNotificationList(member.getId(), page, size);
     return ApiResponse.onSuccess(NotificationConverter.notificationListDTO(notificationList));
   }
 }
