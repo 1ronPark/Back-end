724f9607310aee5dfb0ee7f93eb9077c7c4f2689
HarryBae1011 <06harry@naver.com>
2025-07-20 08:54:16 +0900

    chore: 실수로 삭제한 메서드 원복

--- src/main/java/umc/lightup/skill/repository/SkillRepository.java
+++ src/main/java/umc/lightup/skill/repository/SkillRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import umc.lightup.member.domain.Member;
 import umc.lightup.skill.domain.Skill;
 
 import java.util.List;
@@ -14,4 +15,6 @@ public interface SkillRepository extends JpaRepository<Skill, Long> {
     List<Skill> findBasicSkills();
     @Query("select count(*) from Skill where name = :name and isCustom = false")
     Long countByNameAndIsCustomFalse (@Param("name") String name);
+    @Query("select s.name from MemberSkill ms join ms.skill s where ms.member=:member")
+    List<String> findNameByMember(@Param("member") Member member);
 }
--- src/main/java/umc/lightup/strength/repository/StrengthRepository.java
+++ src/main/java/umc/lightup/strength/repository/StrengthRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import umc.lightup.member.domain.Member;
 import umc.lightup.strength.domain.Strength;
 
 import java.util.List;
@@ -14,4 +15,6 @@ public interface StrengthRepository extends JpaRepository<Strength, Long> {
     List<Strength> findBasicStrengths();
     @Query("select count(*) from Strength where name = :name and isCustom = false")
     Long countByNameAndIsCustomFalse (@Param("name") String name);
+    @Query("select s.name from MemberStrength ms join ms.strength s where ms.member=:member")
+    List<String> findNameByMember(@Param("member") Member member);
 }
