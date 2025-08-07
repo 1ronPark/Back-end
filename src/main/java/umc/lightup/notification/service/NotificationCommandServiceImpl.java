eaa929bd9e20960fb445a945c292e3a391029581
choikyungsoo <ryan1954@naver.com>
2025-08-07 14:12:15 +0900

    refactor: 코드리뷰에 맞게 수정

--- src/main/java/umc/lightup/notification/service/NotificationCommandServiceImpl.java
+++ src/main/java/umc/lightup/notification/service/NotificationCommandServiceImpl.java
@@ -1,9 +1,14 @@
 package umc.lightup.notification.service;
 
+import java.util.Map;
+
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
 import umc.lightup.api.code.status.ErrorStatus;
 import umc.lightup.exception.handler.GeneralHandler;
 import umc.lightup.member.domain.Member;
@@ -14,18 +19,19 @@ import umc.lightup.notification.dto.NotificationEventRequestDTO;
 import umc.lightup.notification.dto.NotificationEventResponseDTO;
 import umc.lightup.notification.repository.NotificationRepostiory;
 
-import java.util.Map;
-
 @Service
 @RequiredArgsConstructor
 public class NotificationCommandServiceImpl implements NotificationCommandService {
+
   private final MemberRepository memberRepository;
   private final NotificationRepostiory notificationRepository;
   private final EntityManager entityManager;
 
   @Override
   public Notification deleteNotification(long notificationId) {
-    Notification notification = notificationRepository.findById(notificationId).orElseThrow(() -> new GeneralHandler(ErrorStatus.NOTIFICATION_NOT_FOUND));
+    Notification notification = notificationRepository.findById(notificationId)
+            .orElseThrow(() -> new GeneralHandler(ErrorStatus.NOTIFICATION_NOT_FOUND));
+
     notificationRepository.deleteById(notificationId);
     return notification;
   }
@@ -33,20 +39,27 @@ public class NotificationCommandServiceImpl implements NotificationCommandServic
   @Override
   @Transactional
   public Notification updateNotification(long notificationId) {
-    Notification notification = notificationRepository.findById(notificationId).orElseThrow(() -> new GeneralHandler(ErrorStatus.NOTIFICATION_NOT_FOUND));
+    Notification notification = notificationRepository.findById(notificationId)
+            .orElseThrow(() -> new GeneralHandler(ErrorStatus.NOTIFICATION_NOT_FOUND));
+
     notification.setIsRead(true);
     return notification;
   }
 
   @Override
   @Transactional
-  public NotificationEventResponseDTO.NotificationEventDTO saveNotification(NotificationEventRequestDTO.NotificationEventDTO notificationEvent) {
-    Member sender = memberRepository.findById(notificationEvent.getSenderId()).orElseThrow(() -> new GeneralHandler(ErrorStatus.MEMBER_NOT_FOUND));
-    Member receiver = memberRepository.findById(notificationEvent.getReceiverId()).orElseThrow(() -> new GeneralHandler(ErrorStatus.MEMBER_NOT_FOUND));
+  public NotificationEventResponseDTO.NotificationEventDTO saveNotification(
+          NotificationEventRequestDTO.NotificationEventDTO notificationEvent
+  ) {
+    Member sender = memberRepository.findById(notificationEvent.getSenderId())
+            .orElseThrow(() -> new GeneralHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+    Member receiver = memberRepository.findById(notificationEvent.getReceiverId())
+            .orElseThrow(() -> new GeneralHandler(ErrorStatus.MEMBER_NOT_FOUND));
 
     Notification notification = NotificationConverter.notificationEventDTO(sender, receiver, notificationEvent);
     notificationRepository.save(notification);
+
     return NotificationConverter.notificationEventDTO(receiver, notification);
   }
-
 }
