88e3f5576b744770d6eab36dcc320d8286f83fb9
cau20232907 <hs20232907@cau.ac.kr>
2025-08-11 15:04:59 +0900

    feat: oauth2 callback 기능 구현

--- src/main/java/umc/lightup/member/service/CredentialQueryService.java
+++ src/main/java/umc/lightup/member/service/CredentialQueryService.java
@@ -10,7 +10,9 @@ public interface CredentialQueryService {
     Member joinMember(MemberRequestDTO.JoinDto request);
     MemberResponseDTO.LoginResultDTO loginMember(MemberRequestDTO.PasswordLoginRequestDTO request);
     MemberResponseDTO.LoginResultDTO loginMemberByGoogle(String authCode);
+    MemberResponseDTO.LoginResultDTO callbackMemberByGoogle(String authCode);
     MemberResponseDTO.LoginResultDTO loginMemberByKakao(String authCode);
+    MemberResponseDTO.LoginResultDTO callbackMemberByKakao(String authCode);
     Member joinMemberByGoogle(String authCode);
     Member joinMemberByKakao(String authCode);
     Member addGoogleLogin(Member member, String authCode);
